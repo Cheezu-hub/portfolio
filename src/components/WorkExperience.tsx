2f83b14f5a92bdc2111e3c9aa5ab338abfcb70a7
Cheezu-hub <rajanachaitanya29@gmail.com>
2026-05-19 20:08:32 +0530

    feat: setup basic WorkExperience component skeleton

---
+++ src/components/WorkExperience.tsx
@@ -0,0 +1,9 @@
+export function WorkExperience() {
+  return (
+    <section className="py-20 px-4 max-w-7xl mx-auto w-full group/experience">
+        <h2 className="text-3xl md:text-4xl font-bold pb-2 bg-clip-text text-transparent bg-gradient-to-r from-neutral-200 to-neutral-500 mb-10">
+          Work Experience
+        </h2>
+    </section>
+  );
+}
